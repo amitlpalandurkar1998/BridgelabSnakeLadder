b6ae945be9bd2cd54ae1d5de92cb04b01d90bc18
Amit <amitlpalandurkar1998@gmail.com>
2023-06-23 22:32:52 +0530

    check winning position 100.

--- src/com/bridgelab/snakeladder/SnakeLadder.java
+++ src/com/bridgelab/snakeladder/SnakeLadder.java
@@ -4,64 +4,75 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class SnakeLadder {
+    static boolean winningPosition = true;
     static int rollsTheDie(){
         Random random = new Random();
         return random.nextInt(6)+1;
     }
 
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
         int playerOnePosition = 0;
 
         System.out.println("Welcome To the Snake and Ladder Game.");
         System.out.println("I am Player 1 My position is : "+ playerOnePosition);
 
-        int playerOneDiePoint = rollsTheDie();
-        playerOnePosition+=playerOneDiePoint;
-        System.out.println("before "+playerOnePosition);
-        switch (playerOnePosition){
-            //ladder
-            case 4:
-                playerOnePosition=56;
-                break;
-            case 14:
-                playerOnePosition=55;
-                break;
-            case 12:
-                playerOnePosition=50;
-                break;
-            case 22:
-                playerOnePosition=58;
-                break;
-            case 41:
-                playerOnePosition=79;
-                break;
-            case 54:
-                playerOnePosition=88;
-                break;
+        while (winningPosition){
+            int playerOneDiePoint = rollsTheDie();
+            if ((playerOnePosition+playerOneDiePoint)>=101){
+                
+            }else {
+                playerOnePosition += playerOneDiePoint;
+            }
+            switch (playerOnePosition) {
+                //ladder
+                case 4:
+                    playerOnePosition = 56;
+                    break;
+                case 14:
+                    playerOnePosition = 55;
+                    break;
+                case 12:
+                    playerOnePosition = 50;
+                    break;
+                case 22:
+                    playerOnePosition = 58;
+                    break;
+                case 41:
+                    playerOnePosition = 79;
+                    break;
+                case 54:
+                    playerOnePosition = 88;
+                    break;
 
-            //Snake
-            case 31:
-                playerOnePosition=3;
-                break;
-            case 28:
-                playerOnePosition=10;
-                break;
-            case 47:
-                playerOnePosition=16;
-                break;
-            case 75:
-                playerOnePosition=32;
-                break;
-            case 94:
-                playerOnePosition=71;
-                break;
-            case 96:
-                playerOnePosition=42;
-                break;
+                //Snake
+                case 31:
+                    playerOnePosition = 3;
+                    break;
+                case 28:
+                    playerOnePosition = 10;
+                    break;
+                case 47:
+                    playerOnePosition = 16;
+                    break;
+                case 75:
+                    playerOnePosition = 32;
+                    break;
+                case 94:
+                    playerOnePosition = 71;
+                    break;
+                case 96:
+                    playerOnePosition = 42;
+                    break;
+            }
+
+            System.out.println("Current Position of Player One is : " + playerOnePosition);
+
+            if (playerOnePosition==100){
+                System.out.println("The Player One Winning The Game.");
+                winningPosition = false;
+            }
         }
-
-
-        System.out.println("Current Position of Player One is : "+ playerOnePosition);
     }
 }
